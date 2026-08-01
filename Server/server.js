5f24e837fc035f88a88dd1093e37e809f52e3aa5
vijaykr2004 <vk8287215362@gmail.com>
2026-08-01 14:19:47 +0530

    Update CORS configuration

--- Server/server.js
+++ Server/server.js
@@ -14,7 +14,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
